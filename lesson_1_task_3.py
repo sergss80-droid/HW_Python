a8dfe3f6dff30aeeb018f47774a4eede9a8f8e64
sergss80-droid <sergss80@gmail.com>
2026-05-30 10:27:30 +0500

    ДЗ - исправление зададаний №3, №4

--- lesson_1_task_3.py
+++ lesson_1_task_3.py
@@ -1,3 +1,3 @@
 first_name = input("Введите ваше имя: ")
 last_name = input("Введите вашу фамилию: ")
-print("Меня зовут:", last_name, first_name)
+print("Вас зовут: " + last_name + " " + first_name)
